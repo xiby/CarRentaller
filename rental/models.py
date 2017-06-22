44e8ab019ef2e496b7d9e045bfd4f305a58f8ba8
xiby <15555438336@163.com>
2017-06-22 08:19:03 +0800

    完成与MySQL的连接，基本表还没有建立

--- rental/models.py
+++ rental/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Test(models.Model):
+    id=models.CharField(primary_key=True,max_length=30)
+    sn=models.CharField(max_length=30)
+    extra=models.CharField(max_length=10)
+    class Meta:
+        verbose_name=('test')
