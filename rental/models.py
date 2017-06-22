cbf0fd94b384f276789290b814a9accc3fb99065
xiby <15555438336@163.com>
2017-06-22 08:35:02 +0800

    完成Store表的建立

--- rental/models.py
+++ rental/models.py
@@ -8,3 +8,14 @@ class Test(models.Model):
     extra=models.CharField(max_length=10)
     class Meta:
         verbose_name=('test')
+
+class Store(models.Model):
+    Snumber=models.CharField(primary_key=True,max_length=20)
+    Sname=models.CharField(max_length=10)
+    Saddress=models.CharField(max_length=50)
+    Stel=models.CharField(max_length=13)
+    Sstime=models.TimeField()
+    Setime=models.TimeField()
+    SExist=models.BooleanField()
+    class Meta:
+        verbose_name=('test')
