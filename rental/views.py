f7c1d6fbb30d5ad73ac94e5e2352916709b25f01
xiby <15555438336@163.com>
2017-06-24 14:38:09 +0800

    实现登陆判断

--- rental/views.py
+++ rental/views.py
@@ -5,22 +5,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.shortcuts import render,redirect
-from . import models
+from django.core.exceptions import ObjectDoesNotExist
+from rental.models import Worker
+# import models
 
 def index(request):
     return render(request,'login.html')
 
 def login(request):
-    # print(request.POST)
-    for item in request.POST:
-        print(item)
     if request.method=='GET':
         return render(request,'login.html')
 
-    if request.method=='POST':
-        username=request.POST['username']
-        if request.POST['password']=='123' and 'worker'== request.POST['job']:
-            return HttpResponse("log in success!")
+    if request.method=='POST' and 'login' in request.POST:
+        print('posting')
+        userid=request.POST['userid']
+        usertittle=request.POST['job']
+        # usersn=Worker.objects.get(id=userid).objects().values('Wsecret')
+        # sql='select Wsecret from rental_worker where id='+str(userid)
+        # usersn=request.POST['password']
+        
+        # print(usersn)
+        # if user==request.POST['password']:
+        #     return HttpResponse('登陆成功')
+        # else:
+        #     return HttpResponse('密码输入错误')
+        if Worker.objects.filter(id=userid,Wtittle=usertittle).exists():
+            user=Worker.objects.get(id=userid,Wtittle=usertittle)
+            if user.Wsecret==request.POST['password']:
+                return HttpResponse('登陆成功')
+            else:
+                return HttpResponse('密码错误')
         else:
-            return HttpResponse("log in failed")
-    
+            return HttpResponse('用户不存在')
